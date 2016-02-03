53b5b09f95d3cb64418efcae5dbbc0c3790ad477
Hugo de Paix de Coeur <hugo.depaixdecoeur@outscale.com>
2016-02-03 19:21:52 +0100

    Cleanups

---
+++ src/main/java/io/github/robwin/markup/builder/TableColumnSpec.java
@@ -0,0 +1,24 @@
+package io.github.robwin.markup.builder;
+
+class TableColumnSpec {
+    public String header;
+    public Integer widthRatio = 0;
+
+    public TableColumnSpec() {
+    }
+
+    public TableColumnSpec(String header, Integer widthRatio) {
+        this.header = header;
+        this.widthRatio = widthRatio;
+    }
+
+    public TableColumnSpec withHeader(String header) {
+        this.header = header;
+        return this;
+    }
+
+    public TableColumnSpec withWidthRatio(Integer widthRatio) {
+        this.widthRatio = widthRatio;
+        return this;
+    }
+}
