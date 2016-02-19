d9392dacc41ae53541f89237e4d0ebf882bf19fd
Hugo de Paix de Coeur <hugo.depaixdecoeur@outscale.com>
2016-02-19 15:36:27 +0100

    Introduce Markup-dependent specifiers on tables

--- src/main/java/io/github/robwin/markup/builder/MarkupTableColumn.java
+++ src/main/java/io/github/robwin/markup/builder/MarkupTableColumn.java
@@ -1,24 +1,65 @@
 package io.github.robwin.markup.builder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MarkupTableColumn {
     public String header;
     public Integer widthRatio = 0;
+    public Map<MarkupLanguage, String> markupSpecifiers = new HashMap<>();
 
+    /**
+     * Empty constructor
+     */
     public MarkupTableColumn() {
     }
 
+    /**
+     * Header constructor
+     * @param header header name
+     */
+    public MarkupTableColumn(String header) {
+        this.header = header;
+    }
+
+    /**
+     * Header and specifiers constructor
+     * @param header header name
+     * @param widthRatio width ratio
+     */
     public MarkupTableColumn(String header, Integer widthRatio) {
         this.header = header;
         this.widthRatio = widthRatio;
     }
 
+    /**
+     * Set header name for this column
+     * @param header header name
+     * @return this builder
+     */
     public MarkupTableColumn withHeader(String header) {
         this.header = header;
         return this;
     }
 
+    /**
+     * Set column width ratio for this column
+     * @param widthRatio width ratio integer value [0-100]. Accept relative width specifiers [0-9] for languages supporting it.
+     * @return this builder
+     */
     public MarkupTableColumn withWidthRatio(Integer widthRatio) {
         this.widthRatio = widthRatio;
+        return this;
+    }
+
+    /**
+     * Overrides all other specifiers (for the specified language) with this language-dependent {@code specifiers} string
+     * @param language apply the {@code specifiers} to this language only
+     * @param specifiers RAW language-dependent specifiers for the column
+     * @return this builder
+     */
+    public MarkupTableColumn withMarkupSpecifiers(MarkupLanguage language, String specifiers) {
+        this.markupSpecifiers.put(language, specifiers);
         return this;
     }
 }
