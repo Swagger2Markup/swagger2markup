07cdcfd3bc55c81c7833e50fa2fe52de045117a4
Robert Winkler <robert.winkler@telekom.de>
2016-03-18 10:36:17 +0100

    Adapted group id, versioning and root package to other swagger2markup projects

--- src/main/java/io/github/robwin/markup/builder/MarkupAdmonition.java
+++
@@ -1,9 +0,0 @@
-package io.github.robwin.markup.builder;
-
-public enum MarkupAdmonition {
-    NOTE,
-    TIP,
-    IMPORTANT,
-    CAUTION,
-    WARNING
-}
--- src/main/java/io/github/robwin/markup/builder/MarkupBlockStyle.java
+++
@@ -1,9 +0,0 @@
-package io.github.robwin.markup.builder;
-
-public enum MarkupBlockStyle {
-    SIDEBAR,
-    EXAMPLE,
-    LITERAL,
-    LISTING,
-    PASSTHROUGH
-}
