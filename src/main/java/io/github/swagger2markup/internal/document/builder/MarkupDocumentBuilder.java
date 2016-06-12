592446f7142f04a02233200761f36d0438620c1f
Johannes Fiala <johannes.fiala@fwd.at>
2016-06-12 17:05:45 +0200

    Removed quotes for Minlength/Maxlength #147

--- src/main/java/io/github/swagger2markup/internal/document/builder/MarkupDocumentBuilder.java
+++ src/main/java/io/github/swagger2markup/internal/document/builder/MarkupDocumentBuilder.java
@@ -238,19 +238,19 @@ public abstract class MarkupDocumentBuilder {
 
                 	if (isNotBlank(description))
                         descriptionContent.newLine(true);
-                    descriptionContent.boldText(LENGTH_COLUMN).text(COLON).literalText(Json.pretty(minlength + " - " + maxlength));
+                    descriptionContent.boldText(LENGTH_COLUMN).text(COLON).literalText(minlength + " - " + maxlength);
                     
                 } else {
                 	 if(minlength != null){
                      	if (isNotBlank(description))
                              descriptionContent.newLine(true);
-                         descriptionContent.boldText(MINLENGTH_COLUMN).text(COLON).literalText(Json.pretty(minlength));
+                         descriptionContent.boldText(MINLENGTH_COLUMN).text(COLON).literalText(minlength.toString());
                      }
                      
                      if(maxlength != null){
                      	if (isNotBlank(description))
                              descriptionContent.newLine(true);
-                         descriptionContent.boldText(MAXLENGTH_COLUMN).text(COLON).literalText(Json.pretty(maxlength));
+                         descriptionContent.boldText(MAXLENGTH_COLUMN).text(COLON).literalText(maxlength.toString());
                      }
                 }
                 
@@ -265,13 +265,13 @@ public abstract class MarkupDocumentBuilder {
                 if(minValue != null){
                 	if (isNotBlank(description))
                         descriptionContent.newLine(true);
-                    descriptionContent.boldText(MINVALUE_COLUMN).text(COLON).literalText(Json.pretty(minValue));
+                    descriptionContent.boldText(MINVALUE_COLUMN).text(COLON).literalText(minValue.toString());
                 }
                 
                 if(maxValue != null){
                 	if (isNotBlank(description))
                         descriptionContent.newLine(true);
-                    descriptionContent.boldText(MAXVALUE_COLUMN).text(COLON).literalText(Json.pretty(maxValue));
+                    descriptionContent.boldText(MAXVALUE_COLUMN).text(COLON).literalText(maxValue.toString());
                 }
                 
                 if (example != null) {
