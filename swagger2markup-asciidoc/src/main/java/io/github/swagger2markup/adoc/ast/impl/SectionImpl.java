2ec02088ceefdda4aec6bdbea70fefc5da41261f
Ali Ustek <austek@mdsol.com>
2020-02-14 07:23:04 +0000

    fix `Overview` title level (#403)

--- swagger2markup-asciidoc/src/main/java/io/github/swagger2markup/adoc/ast/impl/SectionImpl.java
+++ swagger2markup-asciidoc/src/main/java/io/github/swagger2markup/adoc/ast/impl/SectionImpl.java
@@ -3,6 +3,8 @@ package io.github.swagger2markup.adoc.ast.impl;
 import org.asciidoctor.ast.Section;
 import org.asciidoctor.ast.StructuralNode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,59 +18,33 @@ public class SectionImpl extends StructuralNodeImpl implements Section {
     private final boolean numbered;
 
     public SectionImpl(StructuralNode parent) {
-        super(parent, "section");
-        this.index = null;
-        this.number = null;
-        this.numeral = "";
-        this.sectionName = "";
-        this.special = false;
-        this.numbered = false;
+        this(parent, new HashMap<>());
     }
 
     public SectionImpl(StructuralNode parent, Map<String, Object> attributes) {
-        super(parent, "section", attributes);
-        this.index = null;
-        this.number = null;
-        this.numeral = "";
-        this.sectionName = "";
-        this.special = false;
-        this.numbered = false;
+        this(parent, "section", null, "");
     }
 
 
     public SectionImpl(StructuralNode parent, String context, Object content, String sectionName) {
-        super(parent, context, content);
-        this.index = null;
-        this.number = null;
-        this.numeral = "";
-        this.sectionName = sectionName;
-        this.special = false;
-        this.numbered = false;
+        this(parent, context, content, null, null, "", sectionName, false, false);
     }
 
-    public SectionImpl(StructuralNode parent, String context, Object content, int index, int number, String numeral, String sectionName, boolean special, boolean numbered) {
-        super(parent, context, content);
-        this.index = index;
-        this.number = number;
-        this.numeral = numeral;
-        this.sectionName = sectionName;
-        this.special = special;
-        this.numbered = numbered;
+    public SectionImpl(StructuralNode parent, String context, Object content, Integer index, Integer number, String numeral,
+                       String sectionName, boolean special, boolean numbered) {
+        this(parent, context, new HashMap<>(), new ArrayList<>(), content, new ArrayList<>(),
+                "", new ArrayList<>(), index, number, numeral, sectionName, special, numbered);
     }
 
-    public SectionImpl(StructuralNode parent, String context, Map<String, Object> attributes, List<String> roles, Object content, List<StructuralNode> blocks, String contentModel, List<String> subs, int index, int number, String numeral, String sectionName, boolean special, boolean numbered) {
-        super(parent, context, attributes, roles, content, blocks, contentModel, subs);
-        this.index = index;
-        this.number = number;
-        this.numeral = numeral;
-        this.sectionName = sectionName;
-        this.special = special;
-        this.numbered = numbered;
+    public SectionImpl(StructuralNode parent, String context, Map<String, Object> attributes, List<String> roles,
+                       Object content, List<StructuralNode> blocks, String contentModel, List<String> subs,
+                       Integer index, Integer number, String numeral, String sectionName, boolean special, boolean numbered) {
+        this(parent, context, attributes, roles, content, blocks, calculateLevel(parent), contentModel, subs, index, number, numeral, sectionName, special, numbered);
     }
 
     public SectionImpl(StructuralNode parent, String context, Map<String, Object> attributes, List<String> roles,
                           Object content, List<StructuralNode> blocks, Integer level, String contentModel, List<String> subs,
-                          int index, int number, String numeral, String sectionName, boolean special, boolean numbered) {
+                          Integer index, Integer number, String numeral, String sectionName, boolean special, boolean numbered) {
         super(parent, context, attributes, roles, content, blocks, level, contentModel, subs);
         this.index = index;
         this.number = number;
