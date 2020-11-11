859312b7d2bc01c0454e754f8a5cc8c5b891fb9a
austek <austek@mdsol.com>
2020-11-11 06:00:16 +0000

    Remove OpenApi2Markup for 1.3.x release

--- openapi2markup/src/main/java/io/github/swagger2markup/OpenSchema2MarkupConfig.java
+++
@@ -1,7 +0,0 @@
-package io.github.swagger2markup;
-
-import io.github.swagger2markup.config.builder.Schema2MarkupConfigBuilder;
-
-public class OpenSchema2MarkupConfig extends Schema2MarkupConfigBuilder.DefaultSchema2MarkupConfig {
-
-}
